9cc3b10160c77ba890fdad59ab1e1d318a1e7887
Andyeeeee <127477711+Andyeeeee@users.noreply.github.com>
2023-12-10 17:29:59 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <!-- <div style="background-color: bisque;">1</div>
+  <br> -->
+  <router-view />
+</template>
+
+<script setup>
+//
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,42 @@
+// Composables
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('@/layouts/Font.vue'),
+    children: [
+      {
+        path: '/',
+        name: 'Home',
+        component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
+        meta: {
+          title: '首頁'
+        }
+      },
+      {
+        path: '/aboutme',
+        name: 'AboutMe',
+        component: () => import(/* webpackChunkName:"aboutme" */ '@/views/AboutMe.vue'),
+        meta: {
+          title: '關於我'
+        }
+      },
+      {
+        path: 'myproduct',
+        name: 'MyProduct',
+        component: () => import(/* webpackChunkName:"myproduct" */'@/views/MyProduct'),
+        meta: {
+          title: '我的作品'
+        }
+      }
+    ]
+  }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
